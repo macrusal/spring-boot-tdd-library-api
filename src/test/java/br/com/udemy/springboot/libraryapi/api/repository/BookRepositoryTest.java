a56449dbddb63266312636d5fb26cb162fe99ed5
macrusal <macrusal@gmail.com>
2020-04-26 13:20:52 -0300

    TDD - Criando teste de integração com o banco de dados

---
+++ src/test/java/br/com/udemy/springboot/libraryapi/api/repository/BookRepositoryTest.java
@@ -0,0 +1,42 @@
+package br.com.udemy.springboot.libraryapi.api.repository;
+
+import br.com.udemy.springboot.libraryapi.api.model.repository.BookRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+/**
+ * @author macrusal on 26/04/20
+ * @project library-api
+ */
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@DataJpaTest
+public class BookRepositoryTest {
+
+    @Autowired
+    TestEntityManager entityManager;
+
+    @Autowired
+    BookRepository repository;
+
+    @Test
+    @DisplayName("Deve retornar verdadeiro quando existir um livro na base com o isbn informado")
+    public void returnTrueWhenIsbnExists() {
+
+        //cenario
+        String isbn = "01234560";
+
+        //execucao
+        boolean existsByIsbn = repository.existsByIsbn(isbn);
+
+        //verificacao
+        Assertions.assertThat(existsByIsbn).isTrue();
+    }
+}
